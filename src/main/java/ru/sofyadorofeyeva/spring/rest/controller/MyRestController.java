9fb15148b819d1eff2d4a512d7128c28026b4a69
sloncee <sofya.dorofeyeva@yandex.ru>
2025-08-31 19:35:04 +0300

    add getting all employees and one employee by id

---
+++ src/main/java/ru/sofyadorofeyeva/spring/rest/controller/MyRestController.java
@@ -0,0 +1,33 @@
+package ru.sofyadorofeyeva.spring.rest.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.sofyadorofeyeva.spring.rest.entity.Employee;
+import ru.sofyadorofeyeva.spring.rest.service.EmployeeService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class MyRestController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping("/employees")
+    public List<Employee> showAllEmployees() {
+        List<Employee> allEmployees = employeeService.getAllEmployees();
+
+        return allEmployees;
+    }
+
+    @GetMapping("/employees/{id}")
+    public Employee getEmployee(@PathVariable("id") int id) {
+        Employee employee = employeeService.getEmployee(id);
+
+        return employee;
+    }
+}
